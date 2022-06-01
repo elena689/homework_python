e76118d064fadec6db8fa7b30f2d98aa119013b8
Elena <lesikr@ya.ru>
2022-06-01 22:06:27 +0300

    Задача 5 из дз 3 изм1

--- hw3_zadacha5.py
+++ hw3_zadacha5.py
@@ -1,15 +1,15 @@
 #Дан текстовый файл, содержащий целые числа. Удалить из него все четные числа.
 
 
-with open("file_hw3.txt", "w") as file:
+with open("file_hw3.txt", "w") as enter_text:
     for i in range(20):
-        file.write(str(i))
-        file.write("\n")
+        enter_text.write(str(i))
+        enter_text.write("\n")
  
-with open("file_hw3.txt", 'w') as file_2:
+with open("file_hw3.txt", 'w') as del_text:
     for i in range(20):
         if int(str(i)) % 2 != 0:
-            file_2.write(str(i))
-            file_2.write("\n")
+            del_text.write(str(i))
+            del_text.write("\n")
     
 
